50869016c1c042346ee2c9f629adf9f9d47e5cbc
Nick Barratt <nick.barratt50@gmail.com>
2026-05-15 09:04:21 +0100

    Fix lmic_pins incomplete type error by explicitly defining pinmap structure layout

---
+++ project_config/lmic_project_config.h
@@ -0,0 +1,11 @@
+#ifndef _LMIC_PROJECT_CONFIG_H_
+#define _LMIC_PROJECT_CONFIG_H_
+
+// Manually defining regional layout variables for the UK (EU868 MHz)
+#define CFG_eu868 1
+#define CFG_sx1276_radio 1
+
+// Optimizations for standard microcontrollers
+#define DISABLE_BEACONS
+
+#endif // _LMIC_PROJECT_CONFIG_H_
